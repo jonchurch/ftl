1936d731b3eaaef85f4f95f081cf6df2dacf8972
Jon Church <me@jonchurch.com>
2023-05-28 22:00:38 -0400

    adapt test file to FTL package

--- test.ts
+++ test.ts
@@ -1,36 +1,17 @@
 import fs from 'fs'
 import axios from 'axios'
-import axiosRetry from 'axios-retry'
-import chalk from 'chalk'
-import { Configuration, SystemsApi } from 'spacetraders-api'
-import RequestQueue from './axiosRequestQueue'
-import env from './.env.js'
+import FTL from './dist'
 
-const configuration = new Configuration({
-  // read token from environment variables
-  accessToken: env.TOKEN
-})
 
 // create our axios client
 const instance = axios.create()
 // setup retry
-axiosRetry(instance)
-
  // create rate limit queue
-const rateLimitedAxiosQueue = new RequestQueue({
+const rateLimitedAxiosQueue = new FTL({
   maxRequestsPerSecond: 4,
   burstRequests: 10,
   burstTime: 10,
   instance
-})
-
-// pass axios instance with rate limit queue
-const systems = new SystemsApi(configuration, undefined, rateLimitedAxiosQueue.getInstance())
-// let currentSystem: System | null = null
-
-process.on('exit', () => {
-  const requestLogs = rateLimitedAxiosQueue.exportRequestLogs()
-  fs.writeFileSync('run.json', JSON.stringify(requestLogs, null, 2));
 })
 
 async function run() {
